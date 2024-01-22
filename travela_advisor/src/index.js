d4fb7610246248dec7f87fa3cf2a92dba71bec1a
Gupta, Harshit Hemant <hhgupta@iu.edu>
2024-01-21 21:58:18 -0500

    Initialization of Application

---
+++ travela_advisor/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App'; //mount the app to root 
+
+ReactDOM.render(<App/>,document.getElementById('root')); // root is the root node of react
+
